f2b7cd8d8aaaaa1dfc8c095da93261435400bf1f
208937 <208937@ibsplc.com>
2026-08-07 12:31:19 +0530

    codes changed

---
+++ src/main/java/org/ibs/routes/ActiveMqReceiverRouter.java
@@ -0,0 +1,19 @@
+package org.ibs.routes;
+
+
+
+import jakarta.enterprise.context.ApplicationScoped;
+import org.apache.camel.builder.RouteBuilder;
+
+
+@ApplicationScoped
+public class ActiveMqReceiverRouter extends RouteBuilder {
+
+    @Override
+    public void configure() {
+
+        from("activemq:queue:my-activemq-queue")
+                .log("Received message from ActiveMQ: ${body}")
+                .to("log:Received-message from ActiveMq");
+    }
+}
